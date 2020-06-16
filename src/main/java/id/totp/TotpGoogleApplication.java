5a35d30b593ee919bcf9311e7e2ab25cb9739f00
Denny Afrizal <denny.afrizal@alfadigital.id>
2020-06-16 14:24:02 +0700

    Project Skeleton

---
+++ src/main/java/id/totp/TotpGoogleApplication.java
@@ -0,0 +1,13 @@
+package id.totp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TotpGoogleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TotpGoogleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/totp/TotpGoogleApplicationTests.java
@@ -0,0 +1,13 @@
+package id.totp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TotpGoogleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
